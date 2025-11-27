97abd634c7c0dd68c6ab158274b7494e45f10c35
SoYeon Park <amypark310@gmail.com>
2025-11-27 21:27:57 +0900

    [CHORE] Initialize test source structure

---
+++ src/test/java/minimanimo/UserManagerTest.java
@@ -0,0 +1,11 @@
+package minimanimo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserManagerTest {
+    @Test
+    void tempTest() {
+        assertTrue(true);
+    }
+}
