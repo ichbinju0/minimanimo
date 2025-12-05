988da770cc748d398bdaf1f91e196b63f2ec62fb
soyeon <amypark310@gmail.com>
2025-12-05 21:17:03 +0900

    [CHORE] made empty game files

--- src/main/java/minimanimo/game/Updown.java
+++ src/main/java/minimanimo/game/Updown.java
@@ -1,5 +1,22 @@
 package minimanimo.game;
 
-public class UpDown {
-    
+import minimanimo.User;
+import java.util.Scanner;
+
+public class UpDown implements MiniGame {
+    @Override
+    public String getGameName() {
+        return "UpDown";
+    }
+
+    @Override
+    public int startGame(User user, Scanner scanner) {
+        System.out.println("Number Up Down game: Not developed yet");
+        return 0;
+    }
+
+    @Override
+    public boolean isLowerScoreBetter() {
+        return true; 
+    }
 }
