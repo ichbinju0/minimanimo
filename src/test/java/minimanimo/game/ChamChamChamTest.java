2ac8a27062ff863740088ca20e6ce3d4de4d17d4
Kwon Sihyun <sihyun0305@naver.com>
2025-12-12 02:12:06 +0900

    [REFACTOR] Refactoring chamchamchamteat

--- src/test/java/minimanimo/game/ChamChamChamTest.java
+++ src/test/java/minimanimo/game/ChamChamChamTest.java
@@ -1,4 +1,4 @@
-package minimanimo;
+package minimanimo.game;
 
 import java.util.Scanner;
 
@@ -8,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import minimanimo.game.ChamChamCham;
+import minimanimo.User;
+
 
 public class ChamChamChamTest {
     // Helper method to simulate user input
