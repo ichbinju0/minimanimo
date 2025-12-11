8b0530e7f24fff60306a75f4b21c8918c5a3c923
Kwon Sihyun <sihyun0305@naver.com>
2025-12-12 00:34:18 +0900

    [REFACTOR]Clean imports in UserManager

--- src/main/java/minimanimo/UserManager.java
+++ src/main/java/minimanimo/UserManager.java
@@ -1,7 +1,13 @@
 package minimanimo;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserManager {
 
